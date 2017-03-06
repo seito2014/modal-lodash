8ed353200dd9071329dfb2d2e21f0b8acf33b19e
Seito Horiguchi <horiguchi_seito@liginc.co.jp>
2017-03-06 16:53:48 +0900

    ctrl disable

--- modal-lodash.js
+++ modal-lodash.js
@@ -10,25 +10,22 @@
         this.$output = params.output ? $(params.output) : $('#js-ml-output');
         this.$buttonLeft = params.buttonLeft ? $(params.buttonLeft) : $('.js-ml-left');
         this.$buttonRight = params.buttonRight ? $(params.buttonRight) : $('.js-ml-right');
-
         this.item = params.item ? params.item : '.ml-modal-item';
         this.$item;
 
+        //class
         this.activeClass = params.activeClass ? params.activeClass : 'is-active';
+        this.disableClass = params.disableClass ? params.disableClass : 'is-disable';
 
         //lodash
         this.template = _.template;
         this.templateSettings = _.templateSettings;
-
         this.templateSettings.interpolate = /\{\{=([\s\S]+?)\}\}/g;
-
         this.$template = params.template ? $('#'+params.template) : $('#js-ml-template');
         this.compile = this.template(this.$template.html());
 
-        //item length
+        //etc
         this.itemLength;
-
-        //index
         this.index = 0;
 
         window.addEventListener('load', this._init(this));
@@ -84,8 +81,17 @@
             }, //add the active class to the new index item
             show: function(index){
                 self.$item.eq(index).addClass(self.activeClass);
-                console.log(self.$item.eq(index));
             }
+        };
+
+        var ctrlDisableClass = function(){
+          if(self.index === 0){
+              self.$buttonRight.removeClass(self.disableClass);
+              self.$buttonLeft.addClass(self.disableClass);
+          } else if(self.index === self.itemLength - 1) {
+              self.$buttonLeft.removeClass(self.disableClass);
+              self.$buttonRight.addClass(self.disableClass);
+          }
         };
 
         var handleClick = {
@@ -99,11 +105,13 @@
                 toggleItem.memoriseIndex(index);
 
                 toggleItem.show(self.index);
+                ctrlDisableClass();
             },
             closeModal: function(){
                 self.$modal.fadeOut();
             },
             movePrev: function(){
+                console.log('hello');
                 toggleItem.hide(self.index);
                 if(self.index > 0){
                     self.index--;
@@ -111,8 +119,10 @@
                     self.index = 0;
                 }
                 toggleItem.show(self.index);
+                ctrlDisableClass();
             },
             moveNext: function(){
+                console.log('hello');
                 toggleItem.hide(self.index);
                 if(self.index < self.itemLength - 1){
                     self.index++;
@@ -120,6 +130,7 @@
                     self.index = self.itemLength - 1;
                 }
                 toggleItem.show(self.index);
+                ctrlDisableClass();
             }
         };
 
@@ -133,11 +144,15 @@
         });
         self.$buttonLeft.on('click', function(e){
             e.preventDefault();
-            handleClick.movePrev();
+            if(self.index > 0){
+                handleClick.movePrev(this);
+            }
         });
         self.$buttonRight.on('click', function(e){
             e.preventDefault();
-            handleClick.moveNext();
+            if(self.index < self.itemLength - 1){
+                handleClick.moveNext(this);
+            }
         });
     };
 
