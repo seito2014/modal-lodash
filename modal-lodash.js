05431b0549853fb52b7013d5f8ccde5fd3a2bb62
Seito Horiguchi <horiguchi_seito@liginc.co.jp>
2017-03-06 15:28:28 +0900

    Fix setup & action

--- modal-lodash.js
+++ modal-lodash.js
@@ -4,8 +4,16 @@
 
     this.ModalLodash = function(params){
 
-        this.trigger = params.trigger ? params.trigger : 'js-ml-trigger';
-        this.contents = params.contents;
+        this.$trigger = params.trigger ? $(params.trigger) : $('.js-ml-trigger');
+        this.$close = params.close ? $(params.close) : $('.js-ml-close');
+        this.$modal = params.modal ? $(params.modal) : $('#js-ml-modal');
+        this.$output = params.output ? $(params.output) : $('#js-ml-output');
+        this.item = params.item ? params.item : '.ml-modal-item';
+        // this.$item = params.item ? this.$modal.find(params.item) : this.$modal.find('.ml-modal-item');
+        this.$item;
+
+        this.activeClass = params.activeClass ? params.activeClass : 'is-active';
+
         // this.template = params.template ? params.template : 'js-ml-template';
 
         // var self = this;
@@ -28,29 +36,56 @@
 
     //発火
     ModalLodash.prototype._init = function(self){
-        this.play(self);
+        this.setUp(self);
+        this.action(self);
     };
 
-    ModalLodash.prototype.play = function(self){
+    ModalLodash.prototype.setUp = function(self) {
 
         var self = self;
 
-        var $trigger = $('.'+self.trigger);
-
-        var handleClick = function(trigger) {
+        var createModalItems = function (trigger) {
 
             var $data = $(trigger).find('input');
             var modalHtml = self.compile($data.data());
 
             console.log(modalHtml);
-            $('body').append(modalHtml);
+            $(self.$output).append(modalHtml);
         };
 
-        $trigger.on('click', function(e){
-            e.preventDefault();
-            handleClick(this);
-        });
+        var itemLength = self.$trigger.length;
+        var i = 0;
+        while (i < itemLength) {
+            createModalItems(self.$trigger.eq(i));
+            i += 1;
+        }
 
+        self.$item = self.$modal.find(self.item);
+    };
+
+    ModalLodash.prototype.action = function(self) {
+
+        var self = self;
+
+        var handleClick = {
+            openModal: function(trigger){
+                self.$modal.fadeIn();
+                var index = self.$trigger.index(trigger);
+                self.$item.eq(index).addClass(self.activeClass);
+            },
+            closeModal: function(){
+                self.$modal.fadeOut();
+            }
+        };
+
+        self.$trigger.on('click', function(e){
+            e.preventDefault();
+            handleClick.openModal(this);
+        });
+        self.$close.on('click', function(e){
+            e.preventDefault();
+            handleClick.closeModal();
+        });
     };
 
 }.call(this));
